b0a5f1b817797a8bf80eb4f358d110ee245cdd37
UbHTKcQh <lyyubava@gmail.com>
2021-04-20 11:20:11 +0300

    added render tamplate

--- app/view.py
+++ app/view.py
@@ -1,7 +1,8 @@
 from app import app
-
+from flask import render_template
 
 # we use the route() decorator to tell Flask what URL should trigger our function.
 @app.route('/')
 def index():
-    return 'Hello world'
+    name = 'Ivan'
+    return render_template('index.html', n=name)
