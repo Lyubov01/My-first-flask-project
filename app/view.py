571934027c9b80010888ff5374ef5185af4a5736
UbHTKcQh <lyyubava@gmail.com>
2021-04-22 22:21:34 +0300

    added view for error

--- app/view.py
+++ app/view.py
@@ -1,8 +1,16 @@
 from app import app
 from flask import render_template
 
+
 # we use the route() decorator to tell Flask what URL should trigger our function.
+
+
 @app.route('/')
 def index():
     name = 'Ivan'
     return render_template('index.html', n=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
