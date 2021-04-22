8803bd4db531615c33c2970b32a309a4988eba7f
UbHTKcQh <lyyubava@gmail.com>
2021-04-22 15:44:33 +0300

    added login_required

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -7,9 +7,11 @@ from app import db
 from flask import redirect
 from flask import url_for
 posts = Blueprint('posts', __name__, template_folder='templates')
+from flask_security import login_required
 
 
 @posts.route('/create', methods=['POST', 'GET'])
+@login_required
 def create_post():
     if request.method == 'POST':
         title = request.form['title']
@@ -26,6 +28,7 @@ def create_post():
 
 
 @posts.route('/<slug>/edit', methods=['POST', 'GET'])
+@login_required
 def edit_post(slug):
     post = Post.query.filter(Post.slug == slug).first()
     if request.method == 'POST':
