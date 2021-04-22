1da6fd94b06480ca7fc69d07cc3cc48623ebb998
UbHTKcQh <lyyubava@gmail.com>
2021-04-22 15:43:30 +0300

    added databases for users and roles

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from app import db
 from datetime import datetime
 import re
 
+from flask_security import UserMixin, RoleMixin
+
 
 # make understandable url for title using re
 def slugify(s):
@@ -49,3 +51,24 @@ class Tag(db.Model):
         return "<Tag id: {}, name: {}>".format(self.id, self.name)
 
 
+# Flask security!
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+                       )
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    # backref keyword is merely a shortcut for building two individual relationship()
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
